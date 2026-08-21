9d7889a288ec9bd13e71392747fc77a37d0ea8ff
Pavana94 <soddalapavana9@gmail.com>
2026-08-21 19:36:27 +0530

    Add Operators.java

---
+++ Operators.java
@@ -0,0 +1,60 @@
+public class Operators {
+    public static void main(String[] args) {
+
+        System.out.println("==========================================");
+        System.out.println("              JAVA OPERATORS");
+        System.out.println("==========================================");
+
+        System.out.println("\n===== Arithmetic Operators =====");
+        System.out.println("+  Addition");
+        System.out.println("-  Subtraction");
+        System.out.println("*  Multiplication");
+        System.out.println("/  Division");
+        System.out.println("%  Modulus");
+
+        System.out.println("\n===== Unary Operators =====");
+        System.out.println("+  Unary Plus");
+        System.out.println("-  Unary Minus");
+        System.out.println("++ Increment");
+        System.out.println("-- Decrement");
+        System.out.println("!  Logical NOT");
+
+        System.out.println("\n===== Assignment Operators =====");
+        System.out.println("=   Assignment");
+        System.out.println("+=  Add and Assign");
+        System.out.println("-=  Subtract and Assign");
+        System.out.println("*=  Multiply and Assign");
+        System.out.println("/=  Divide and Assign");
+        System.out.println("%=  Modulus and Assign");
+
+        System.out.println("\n===== Relational Operators =====");
+        System.out.println("==  Equal to");
+        System.out.println("!=  Not Equal to");
+        System.out.println(">   Greater than");
+        System.out.println("<   Less than");
+        System.out.println(">=  Greater than or Equal to");
+        System.out.println("<=  Less than or Equal to");
+
+        System.out.println("\n===== Logical Operators =====");
+        System.out.println("&&  Logical AND");
+        System.out.println("||  Logical OR");
+        System.out.println("!   Logical NOT");
+
+        System.out.println("\n===== Ternary Operator =====");
+        System.out.println("? : Conditional Operator");
+
+        System.out.println("\n===== Bitwise Operators =====");
+        System.out.println("&   Bitwise AND");
+        System.out.println("|   Bitwise OR");
+        System.out.println("^   Bitwise XOR");
+        System.out.println("~   Bitwise NOT");
+        System.out.println("<<  Left Shift");
+        System.out.println(">>  Right Shift");
+
+        System.out.println("\n===== Type Casting =====");
+        System.out.println("Widening Casting");
+        System.out.println("Narrowing Casting");
+
+        System.out.println("\n==========================================");
+    }
+}
