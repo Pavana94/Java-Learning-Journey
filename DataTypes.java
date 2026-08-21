88566d6941473d49f84e3e4de1049bced781eaf0
Pavana94 <soddalapavana9@gmail.com>
2026-08-21 19:35:47 +0530

    Add DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,21 @@
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        String name = "Pavana";
+        int age = 19;
+        float height = 4.21f;
+        char gender = 'F';
+        boolean isStudent = true;
+        double cgpa = 8.866666666;
+
+        System.out.println("***********************************");
+        System.out.println("Name       : " + name);
+        System.out.println("Age        : " + age);
+        System.out.println("Height     : " + height);
+        System.out.println("Gender     : " + gender);
+        System.out.println("Is Student : " + isStudent);
+        System.out.println("CGPA       : " + cgpa);
+        System.out.println("***********************************");
+    }
+}
