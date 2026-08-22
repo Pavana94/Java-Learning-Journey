59fa3d73d0a1797f1cd08b3d1970d1f8f7e196d7
Pavana94 <soddalapavana9@gmail.com>
2026-08-22 20:11:59 +0530

    Relational.java

---
+++ Relational.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Relational {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("\nRelational Operations");
+        System.out.println("---------------------");
+
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+        System.out.println("a > b  : " + (a > b));
+        System.out.println("a < b  : " + (a < b));
+        System.out.println("a >= b : " + (a >= b));
+        System.out.println("a <= b : " + (a <= b));
+
+        sc.close();
+    }
+}
